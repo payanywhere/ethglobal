da38880df6385c714f849a2f3e3d5465518def7e
Tomas M. Di Mauro <tomas@tdm.ar>
2025-11-22 14:51:20 -0300

    [feat] added receive tokens modal in merchant wallet page

--- front/src/app/dashboard/wallet/components/index.ts
+++ front/src/app/dashboard/wallet/components/index.ts
@@ -2,4 +2,5 @@ export { WalletHeader } from "./wallet-header"
 export { TokenBalancesList } from "./token-balances-list"
 export { TokenBalanceItem } from "./token-balance-item"
 export { TransactionHistory } from "./transaction-history"
+export { ReceiveOverlay } from "./receive-overlay"
 
