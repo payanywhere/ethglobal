7fc03c9659e0d00607c0c6c45317501238e37d90
Leo Sagan <raiseerco@gmail.com>
2025-11-22 22:21:42 -0300

    chore: fixes consolidate query

--- back/src/interface/http/routes/consolidate-routes.ts
+++ back/src/interface/http/routes/consolidate-routes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express"
-import { consolidate, getPending } from "../controllers/consolidate-controller"
+import { getPending } from "../controllers/consolidate-controller"
 
 const router = Router()
 
-router.post("/consolidate", consolidate)
 router.get("/consolidate", getPending)
 
 export default router
