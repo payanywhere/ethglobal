bdb9584b107ab0c655d18b9e02affde4b33113d3
Tomas M. Di Mauro <tomas@tdm.ar>
2025-11-23 01:37:34 -0300

    [build] updated env vars in config files

--- infra/modules/cicd/main.tf
+++ infra/modules/cicd/main.tf
@@ -17,8 +17,12 @@ resource "google_cloudbuild_trigger" "back" {
   filename = "back/cloudbuild.yaml"
 
   substitutions = {
-    _MONGO_URI    = var.mongo_uri
-    _SERVICE_NAME = "back-service"
+    _MONGO_URI     = var.mongo_uri
+    _SERVICE_NAME  = "back-service"
+    _RPC_PROVIDER  = var.rpc_provider
+    _PRIVATE_KEY   = var.private_key
+    _PORT          = tostring(var.port)
+    _APP_ENV       = var.next_public_environment
   }
 }
 
@@ -41,8 +45,19 @@ resource "google_cloudbuild_trigger" "front" {
   filename = "front/cloudbuild.yaml"
 
   substitutions = {
-    _SERVICE_NAME        = "front-service"
-    _NEXT_PUBLIC_API_URL = "https://${var.domain_name}"
+    _SERVICE_NAME                    = "front-service"
+    _NEXT_PUBLIC_API_URL             = var.next_public_api_url
+    _NEXT_PUBLIC_BASE_URL            = var.next_public_base_url
+    _NEXT_PUBLIC_ENVIRONMENT         = var.next_public_environment
+    _MP_PUBLIC_KEY                   = var.mp_public_key
+    _MP_ACCESS_TOKEN                 = var.mp_access_token
+    _NEXT_PUBLIC_DYNAMIC_ENV_ID      = var.next_public_dynamic_env_id
+    _CDP_API_KEY_APPID               = var.cdp_api_key_appid
+    _CDP_API_KEY_SECRET              = var.cdp_api_key_secret
+    _CDP_WALLET_SECRET               = var.cdp_wallet_secret
+    _PAYANYWHERE_FEE_COMPOSER_ADDRESS = var.payanywhere_fee_composer_address
+    _APP_ENV                         = var.next_public_environment
+    _BACKEND_API_URL                 = var.next_public_api_url
   }
 }
 
